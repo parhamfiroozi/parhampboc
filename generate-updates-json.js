d95f7bec4d7cbab89980b700cb1f87b77a50fa02
parhamfiroozi <97130452+parhamfiroozi@users.noreply.github.com>
2025-04-05 23:24:35 +0330

    generate-updates-json.js

--- generate-updates-json.js
+++ generate-updates-json.js
@@ -11,11 +11,18 @@ function walk(dir) {
   for (const entry of entries) {
     const fullPath = path.join(dir, entry.name);
 
+    // Ignore these directories
     if (entry.isDirectory() && !['node_modules', '.git', '.github'].includes(entry.name)) {
       walk(fullPath);
-    } else if (entry.isFile() && entry.name.endsWith('.html')) {
+    } 
+    else if (entry.isFile() && entry.name.endsWith('.html')) {
       const stats = fs.statSync(fullPath);
-      const key = path.relative('.', fullPath).replace(/\\/g, '/').replace(/\.html$/, '');
+      // e.g. "electrical/eca1_CENO1.html" => "electrical/eca1_CENO1"
+      const key = path
+        .relative('.', fullPath)
+        .replace(/\\/g, '/')    // Handle Windows backslashes
+        .replace(/\.html$/, '');
+      
       updates[key] = stats.mtime.toISOString();
     }
   }
